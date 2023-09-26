1a0a9240b767b09d58e4bae189df74bd6e3050f7
malamcsc <malamcsc@gmail.com>
2023-09-26 16:56:36 +0530

    fixed

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from waitress import serve
+#from waitress import serve
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return 'Hello world Test app'
+  return 'Hello world Test app for github action'
 
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     app.run(app, host="0.0.0.0", port=8080)
 
